79f55447661731d47a2b292d01d83b6174f278e5
ViktorBarzin <vbarzin@gmail.com>
2018-04-09 22:44:00 +0100

    add rules and game partial interfaces

--- score_calculator/interfaces.py
+++ score_calculator/interfaces.py
@@ -1,11 +1,25 @@
 from interface import Interface
+from typing import List
 
 
 class ICard(Interface):
-    def get_suit(self):
+    def get_suit(self) -> str:
         pass
 
-    def get_rank(self):
+    def get_rank(self) -> str:
         pass
 
 
+class IGame(Interface):
+    pass
+
+
+class Rules(Interface):
+    def get_value_of(card: ICard) -> int:
+        pass
+
+    def get_bonus_points(cards_list: List[ICard]) -> int:
+        '''
+        Returns an integer representing bonus points if any for possessing the input cards.
+        '''
+        pass
