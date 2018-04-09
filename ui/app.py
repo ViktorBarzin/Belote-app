7d687b5f6471463a6cef565874a31502d7247ce6
ViktorBarzin <vbarzin@gmail.com>
2018-04-09 22:32:20 +0100

    ui class renamed to MobileApp

--- ui/app.py
+++ ui/app.py
@@ -1,9 +1,12 @@
 from kivy.app import App
 from kivy.uix.button import Button
 
+from score_calculator import card
+
 
 class MobileApp(App):
     def build(self):
+        card.Card()
         return Button(text='Hello World')
 
 
