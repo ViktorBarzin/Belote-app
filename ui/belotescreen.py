8cf08b2c42a5d538512f44471773c7415e86f980
ViktorBarzin <vbarzin@gmail.com>
2018-04-13 11:17:07 +0100

    camera integration and save image to app data dir

--- ui/belotescreen.py
+++ ui/belotescreen.py
@@ -2,6 +2,10 @@ from ui.mixins import ChangeScreenMixin
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
+# from kivy.core.camera import Camera
+from os.path import exists
+from plyer import camera
 
 
 class BeloteGameScreen(ChangeScreenMixin, Screen):
@@ -13,11 +17,33 @@ class BeloteGameScreen(ChangeScreenMixin, Screen):
         home_screen_btn = Button(text='Home')
         home_screen_btn.bind(on_press=self.change_screen)
         self.items.add_widget(home_screen_btn)
-        # camera_widget = Camera(play=True, resolution=(640, 480))
-        # self.items.add_widget(camera_widget)
-        # button = Button(text='Take Picture', size_hint=(0.12, 0.12))
-        # # button.bind(on_press=self.capture)
-        # self.items.add_widget(button)
+        # print(camera_widget.index)
+        button = Button(text='Scan cards', on_press=self.do_capture)
+        # button.bind(on_press=self.capture)
+        self.items.add_widget(button)
         self.add_widget(self.items)
 
+    def do_capture(self, widget):
+        from plyer import notification
+        notification.notify('in do_capture', 'predi')
+        # picture = camera.take_picture('/Internal storage/Download/pls_work.jpg', on_complete=self.camera_callback)
+        from jnius import autoclass
+        print('BB'*40)
+        print(autoclass('org.kivy.android.PythonActivity').mActivity.getExternalCacheDir().getPath())
+        picture = camera.take_picture(autoclass('org.kivy.android.PythonActivity').mActivity.getExternalCacheDir().getPath() + '/pls.jpg', on_complete=self.camera_callback)
+        notification.notify('in do_capture', 'sled')
 
+    def camera_callback(self, filepath):
+        print('B' * 40 + str(picture))
+        if(exists(filepath)):
+            popup = MsgPopup("Picture saved!")
+            popup.open()
+        else:
+            popup = MsgPopup("Could not save your picture!")
+            popup.open()
+        return True
+
+
+class MyCamera(Camera):
+    def on_load(self):
+        print('aaa')
--- ui/homescreen.py
+++ ui/homescreen.py
@@ -6,7 +6,6 @@ from ui.helpers import LabelContainer
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.camera import Camera
 from ui.belotescreen import BeloteGameScreen
 
 
