fa35369154f9f7ebcb25a5237d1f9897a780d131
ViktorBarzin <vbarzin@gmail.com>
2018-04-14 20:47:24 +0100

    add some spec

--- settings.py
+++
@@ -1,3 +0,0 @@
-IMAGE_NAME = 'capture.jpg'
-
-sgs7_screen_size = {'width': 360, 'height': 640}
--- ui/mixins.py
+++
@@ -1,9 +0,0 @@
-class ChangeScreenMixin:
-    def change_screen(self, widget):
-        # I guess it is a hacky way but should be fine as long as names are
-        # consistent
-        if widget._label._text == 'Belote':
-            self.screen_manager.current = 'belote_game_screen'
-        elif widget._label._text == 'Home':
-            self.screen_manager.current = 'home_screen'
-
